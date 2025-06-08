a47692e68d3ca25e0425a0019d0af58400129f8a
Karol Lukaszczyk <lukkarcontact@gmail.com>
2025-06-08 17:49:58 +0200

    Update Caddyfile and Terraform configuration for HTTPS support and port changes

--- server_deployment/terraform/main.tf
+++ server_deployment/terraform/main.tf
@@ -106,7 +106,7 @@ resource "azurerm_network_security_rule" "allow_superset_caddy" {
   access                     = "Allow"
   protocol                   = "Tcp"
   source_port_range          = "*"
-  destination_port_range     = "8090"
+  destination_port_range     = "443"
   source_address_prefix      = "*"
   destination_address_prefix = "*"
 
